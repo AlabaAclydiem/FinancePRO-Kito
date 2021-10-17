95f47ad86c6753adadf53554bb7dd6ac974fceec
AlabaAclydiem <pastukhou.kirill@gmail.com>
2021-10-17 12:20:49 +0300

    Hello

---
+++ app/src/main/java/com/example/kito/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.kito;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Anton's Page");
+        bt1 = findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAct2();
+            }
+        });
+    }
+
+    public void openAct2() {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    };
+}
