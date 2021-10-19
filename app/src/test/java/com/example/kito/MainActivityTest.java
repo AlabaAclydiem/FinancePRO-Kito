3e46dc44185cade335cb1baca3809bd73131b775
AlabaAclydiem <pastukhou.kirill@gmail.com>
2021-10-19 21:36:20 +0300

    Create MainActivityTest.java

---
+++ app/src/test/java/com/example/kito/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.kito;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testOpenAct2() {
+
+    }
+}
