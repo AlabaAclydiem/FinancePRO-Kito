aae75f3806ea19b9b7adc6a1f0045b4668ab8f8b
AlabaAclydiem <pastukhou.kirill@gmail.com>
2021-10-23 19:46:24 +0300

    Jacoco?

--- app/src/test/java/com/example/kito/CalculatorTest.java
+++ app/src/test/java/com/example/kito/CalculatorTest.java
@@ -1,13 +1,20 @@
 package com.example.kito;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
 
     @Test
     public void evaluatesExpression() {
-        Calculator calculator = new Calculator();
         int sum = calculator.evaluate("1+2+3");
         assertEquals(6, sum);
     }
